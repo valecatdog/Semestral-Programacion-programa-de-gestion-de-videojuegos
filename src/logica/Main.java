e6c8e664bff5e008ee8b997622bec4853a4706fd
57815971 <57815971@T140J2312027023>
2025-06-30 21:16:49 -0300

    Hice el main segun lo que pedia la letra

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -1,9 +1,48 @@
 package logica;
 
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList <Videojuego> listaVideojuegos = new ArrayList<>();
+        String nombre;
+        Year anioLanz;
+        int pegi;
+        String genero;
         
+        for (int i = 0; i < 1; i++){
+            System.out.println("Ingrese el nombre del juego: ");
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el año de lanzamiento del juego: ");
+            anioLanz = Year.of(scan.nextInt());
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el PEGI del juego: ");
+            pegi = scan.nextInt();
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el genero del juego: ");
+            genero = scan.nextLine();
+            
+            Videojuego videojuego = new Videojuego(nombre, pegi, genero, anioLanz);
+            System.out.println(videojuego);
+            listaVideojuegos.add(videojuego);
+        }
+        
+        System.out.println("\n");
+        for (Videojuego v : listaVideojuegos){
+            System.out.println(v);
+        }
+        
+        listaVideojuegos.get(0).setGeneroPorCodigo(3);
+        System.out.println("\n");
+        System.out.println(listaVideojuegos.get(0));
+               
     }
     
 }
